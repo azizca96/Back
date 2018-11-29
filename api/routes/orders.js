eb306f32f18f61ab590ace02be5c82ce3840a92d
azizca96 <arafaaziz6@gmail.com>
2018-11-29 21:00:56 +0100

    update done

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,116 +1,214 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const cryptage_engine = require('../../crypt/cryptage');
+const jwt = require("jsonwebtoken");
 
-const Order = require("../models/order");
-const Product = require("../models/product");
 
-// Handle incoming GET requests to /orders
-router.get("/", (req, res, next) => {
-  Order.find()
-    .select("product quantity _id")
-    .populate('product', 'name')
+const User = require("../models/user").User;
+//  route to signup
+router.post("/signup", (req, res, next) => {
+// we have to encrypt our password
+  let req_email = req.body.email
+  let req_password= req.body.password
+  let crypt_pass = cryptage_engine.encrypt(req_password)
+  let req_first_name = req.body.first_name
+  let req_last_name  = req.body.last_name
+  let req_phone_number = req.body.phone_number
+  let req_birthday= req.body.birthday
+  let req_about= req.body.about
+  let req_cv_link=req.body.cv_link
+  let req_experience=req.body.experience
+  let req_location= req.body.location
+  let req_projects= req.body.projects
+  User.find({ email: req.body.email })
     .exec()
-    .then(docs => {
-      res.status(200).json({
-        count: docs.length,
-        orders: docs.map(doc => {
-          return {
-            _id: doc._id,
-            product: doc.product,
-            quantity: doc.quantity,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/orders/" + doc._id
-            }
-          };
-        })
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
-
-router.post("/", (req, res, next) => {
-  Product.findById(req.body.productId)
-    .then(product => {
-      if (!product) {
-        return res.status(404).json({
-          message: "Product not found"
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail exists"
         });
+      } 
+     
+      
+      else {
+        console.log("yo")
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              first_name: req_first_name,
+              last_name: req_last_name,
+              phone_number: req_phone_number,
+              birthday: req_birthday ,
+              about: req_about,
+              cv_link: req_cv_link,
+              experience: req_experience,
+              location: req_location,
+              projects: req_projects,
+              email: req_email,
+              password:crypt_pass
+
+
+
+            });
+            user
+              .save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created"
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error: err
+                });
+              });  
       }
-      const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId
-      });
-      return order.save();
-    })
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: "Order stored",
-        createdOrder: {
+    });
+
+
+
+
+
+
+  });
+
+
+//route for auth
+router.post("/login",(req , res, next)=> {
+  let req_email = req.body.email
+  let req_password= req.body.password
+  let crypt_pass = cryptage_engine.encrypt(req_password)
+  
+  User.find({email:req_email}).exec()
+  .then(user => {
+    console.log(user)
+
+
+    if(user){
+      console.log("we have a backdata");
+      //we verifie the password into the DB and the password passed into form
+      if ((crypt_pass === user[0].password) && (req_email === user[0].email)) {
+        return res.status(200).json({
+          message: "auth success"
+        })
+      }
+      else{
+        return res.status(200).json({message:"auth fail"})
+      }
+    }
+    else{
+      return res.status(200).json({message:"auth fail"})
+    }
+  })
+  
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err,
+      message:"login or pass dsnt match"
+    });
+    console.log(err)
+  });
+
+})
+ 
+//route for getting all  users in the  data base
+router.get("/getuser", (req, res, next) =>{
+ User.find()
+  .select("email first_name last_name phone_number birthday about cv_link experience location projects _id")
+  .exec()
+  .then(result => {
+    res.status(200).json({
+      count: result.length,
+      users: result.map(result => {
+        return{
           _id: result._id,
-          product: result.product,
-          quantity: result.quantity
-        },
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/orders/" + result._id
+          email: result.email,
+         // password, 
+          first_name: result.first_name,
+          last_name: result.last_name,
+          phone_number: result.phone_number,
+          birthday: result.birthday ,
+          about: result.about,
+          cv_link: result.cv_link,
+          experience: result.experience,
+          location: result.location,
+          projects: result.projects
+      
         }
+      })
+
+    })
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: err
+    })
+  })
+})
+
+// route  for finding  by  id  users
+
+router.get("/getuser/:userId", (req, res, next) =>{
+  User.findById(req.params.userId)
+   //.select("email first_name last_name phone_number birthday about cv_link experience location projects _id")
+   .exec()
+   .then(users => {
+   if(!users){
+     return res.status(404).json({
+       message: "no user found"
+     })
+   }
+   res.status(200).json({
+     user_found: users
+   })
+   })
+   .catch(err => {
+     res.status(500).json({
+       error: err
+     })
+   })
+ })
+
+
+//update user's data
+
+router.patch('/updateuser/:userId', (req , res, next)=> {
+  const id = req.params.userId
+  const updateOps = {}
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  
+  }
+  User.update({_id:id}, {$set: updateOps})
+  .exec()
+  .then(result => {
+    res.status(200).json({
+      message: "update success",
+      //valeur: ops.value
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(400).json({
+      error: err
+    })
+  })
+})
+ 
+//route to delete user by id
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "User deleted"
       });
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
-
-router.get("/:orderId", (req, res, next) => {
-  Order.findById(req.params.orderId)
-    .populate('product')
-    .exec()
-    .then(order => {
-      if (!order) {
-        return res.status(404).json({
-          message: "Order not found"
-        });
-      }
-      res.status(200).json({
-        order: order,
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/orders"
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
-
-router.delete("/:orderId", (req, res, next) => {
-  Order.remove({ _id: req.params.orderId })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: "Order deleted",
-        request: {
-          type: "POST",
-          url: "http://localhost:3000/orders",
-          body: { productId: "ID", quantity: "Number" }
-        }
-      });
-    })
-    .catch(err => {
       res.status(500).json({
         error: err
       });
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,58 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname);
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
 
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
-    .select("name price _id productImage")
     .exec()
     .then(docs => {
-      const response = {
-        count: docs.length,
-        products: docs.map(doc => {
-          return {
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id
-            }
-          };
-        })
-      };
+      console.log(docs);
       //   if (docs.length >= 0) {
-      res.status(200).json(response);
+      res.status(200).json(docs);
       //   } else {
       //       res.status(404).json({
       //           message: 'No entries found'
@@ -67,28 +25,19 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price,
-    productImage: req.file.path 
+    price: req.body.price
   });
   product
     .save()
     .then(result => {
       console.log(result);
       res.status(201).json({
-        message: "Created product successfully",
-        createdProduct: {
-            name: result.name,
-            price: result.price,
-            _id: result._id,
-            request: {
-                type: 'GET',
-                url: "http://localhost:3000/products/" + result._id
-            }
-        }
+        message: "Handling POST requests to /products",
+        createdProduct: result
       });
     })
     .catch(err => {
@@ -102,18 +51,11 @@ router.post("/", upload.single('productImage'), (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
-    .select('name price _id productImage')
     .exec()
     .then(doc => {
       console.log("From database", doc);
       if (doc) {
-        res.status(200).json({
-            product: doc,
-            request: {
-                type: 'GET',
-                url: 'http://localhost:3000/products'
-            }
-        });
+        res.status(200).json(doc);
       } else {
         res
           .status(404)
@@ -135,13 +77,8 @@ router.patch("/:productId", (req, res, next) => {
   Product.update({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-      res.status(200).json({
-          message: 'Product updated',
-          request: {
-              type: 'GET',
-              url: 'http://localhost:3000/products/' + id
-          }
-      });
+      console.log(result);
+      res.status(200).json(result);
     })
     .catch(err => {
       console.log(err);
@@ -156,14 +93,7 @@ router.delete("/:productId", (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then(result => {
-      res.status(200).json({
-          message: 'Product deleted',
-          request: {
-              type: 'POST',
-              url: 'http://localhost:3000/products',
-              body: { name: 'String', price: 'Number' }
-          }
-      });
+      res.status(200).json(result);
     })
     .catch(err => {
       console.log(err);
